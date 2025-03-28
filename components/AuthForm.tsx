82a43ae9ab0f356913d18c0e9d8758c9492eeb11
Yash Mehta <yashmehta0395@gmail.com>
2025-03-28 22:45:50 +0530

    created sign in function

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 import { toast } from 'sonner';
 import FormField from './FormField';
 import { useRouter } from 'next/router';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '@/firebase/client';
 
 const authFormSchema = (type: FormType) => {
@@ -56,6 +56,21 @@ const AuthForm = ({type}: { type: FormType }) => {
                 toast.success("Account created successfully. Please sign in.");
                 router.push("/sign-in")
             } else {
+                const { email, password} = values;
+
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+                const idToken = await userCredential.user.getIdToken();
+
+                if(!idToken) {
+                    toast.error("Sign in Failed")
+                    return;
+                }
+
+                await signIn({
+                    email, idToken
+                })
+                
                 toast.success("Sign in successfully.");
                 router.push("/")
             }
--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { db } from "@/firebase/admin";
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
     const { uid, name, email } = params;
@@ -35,3 +38,42 @@ export async function signUp(params: SignUpParams) {
     }
 }
 
+export async function signIn(params: SignInParams) {
+    const { email, idToken } = params;
+    try {
+        const userRecord = await auth.getUserByEmail(email);
+
+        if(!userRecord) {
+            return {
+                success: false,
+                message: "User does not exist. Create an account instead."
+            }
+        }
+
+        await setSessionCookie(idToken);
+
+    } catch (e) {
+        console.log(e);
+
+        return {
+            success: false,
+            message: "Failed to log into the account."
+        }
+    }
+}
+
+export async function setSessionCookie(idToken: string) {
+    const cookieStore = await cookies();
+
+    const sessionCookie = await auth.createSessionCookie(idToken, {
+        expiresIn: ONE_WEEK * 1000,
+    })
+
+    cookieStore.set("session", sessionCookie, {
+        maxAge: ONE_WEEK,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        path: "/",
+        sameSite: "lax",
+    })
+}
